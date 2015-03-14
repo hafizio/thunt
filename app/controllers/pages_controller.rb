0cd3efbed0f1bd3d81e6f16ba5ebea8d68fff2e2
Hafiz Khairudin <ibnukamy@gmail.com>
2015-03-14 13:23:41 +0800

    Add json

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,8 @@ class PagesController < ApplicationController
   end
 
   def treasures
-    content = File.read(Rails.root.join('app', 'assets', 'json', 'sample.json'))
-    @treasures = JSON.parse(content)
+    @content = File.read(Rails.root.join('app', 'assets', 'json', 'sample.json'))
+    @treasures = ActiveSupport::JSON.decode(@content)
+    @result = HashProxy.create_from(@treasures)
   end
 end
