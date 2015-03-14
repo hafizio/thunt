92dd591f9a09a710b032e43a47719f442cb87ccb
Hafiz Khairudin <ibnukamy@gmail.com>
2015-03-14 12:28:53 +0800

    Add treasures pages;

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def treasures
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  get 'treasures', to: 'pages#treasures',  as: 'treasures'
 end
