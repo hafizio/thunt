435562b7781aa46edbea7842c47a291f7449c198
Hafiz Khairudin <ibnukamy@gmail.com>
2015-03-14 23:25:52 +0800

    add homie

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,10 @@
 class PagesController < ApplicationController
+  layout 'homie', only: [:home]
+
   def home
   end
 
   def treasures
-    @content = File.read(Rails.root.join('app', 'assets', 'json', 'sample.json'))
-    @treasures = ActiveSupport::JSON.decode(@content)
-    @result = HashProxy.create_from(@treasures)
   end
 
   def confirmation
